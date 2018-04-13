d8f14faf036a7cfd2c57debe6f2b34d2cc8f1125
gaolei <gaolei80@gmail.com>
2018-04-13 15:42:07 +0800

    Add Swagger2

--- src/main/java/com/springcloud/clouddemo/web/HelloController.java
+++ src/main/java/com/springcloud/clouddemo/web/HelloController.java
@@ -3,9 +3,14 @@ package com.springcloud.clouddemo.web;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 public class HelloController {
 	
+    @ApiOperation(value="返回hello world", notes="服务提供方")
+    @ApiImplicitParam(name = "null", value = "null", required = false, dataType = "null")
 	@RequestMapping("/hello")
 	public String index() {
 		return "Hello world";
